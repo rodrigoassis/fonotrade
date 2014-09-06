957abcc0a52d001b50c06df0c746f1dd4aa200dd
Rodrigo <rodrigo@dashbid.com>
2014-09-06 16:52:44 -0300

    plan and professional relationship

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,4 +2,13 @@ class Plan < ActiveRecord::Base
   belongs_to :professional, inverse_of: :plans
 
   validates :name, :professional, presence: true
+
+  rails_admin do
+    list do
+      field :name
+      field :professional
+      field :created_at
+      field :updated_at
+    end
+  end
 end
--- app/models/professional.rb
+++ app/models/professional.rb
@@ -1,11 +1,13 @@
 class Professional < ActiveRecord::Base
 
-  has_many :plans, inverse_of: :professionals, dependent: :destroy
+  has_many :plans, inverse_of: :professional, dependent: :destroy
 
   validates :name, :email, :crfa, :cpf, :cep, :state, :city, :address, :neighborhood, presence: true
   validates :cep, format: { with: /\A\d{5}-\d{3}\z/, message: I18n.t('errors.messages.wrong_format', correct_format: '99999-999') }
   validates :cpf, format: { with: /\A\d{3}.\d{3}.\d{3}-\d{2}\z/, message: I18n.t('errors.messages.wrong_format', correct_format: '999.999.999-99') }
   validates :cpf, :crfa, :email, uniqueness: true
+
+  accepts_nested_attributes_for :plans, allow_destroy: true
 
   def city_enum
     I18n.t('cities')
@@ -25,6 +27,12 @@ class Professional < ActiveRecord::Base
       field :state
       field :city
       field :neighborhood
+    end
+
+    modal do
+      configure :plans do
+        hide
+      end
     end
   end
 
