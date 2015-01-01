b5eb4a08f6dd94b2267c7ef6ef5cc515fbd06284
Rodrigo <rodrigo.assis.fialho@gmail.com>
2015-01-01 18:50:24 -0200

    index layout

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'application#root_to_admin'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
